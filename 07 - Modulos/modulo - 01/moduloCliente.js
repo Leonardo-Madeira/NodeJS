5c63d184a602a84fe7b3facb84f102238ef95f76
Leonardo <leomadeiraa@hotmail.com>
2021-07-09 13:57:39 -0300

    Create moduloCliente.js

---
+++ 07 - Modulos/modulo - 01/moduloCliente.js
@@ -0,0 +1,15 @@
+// importar um modulo
+
+const exA = require('./ex=a');
+const exB = require('./ex=b');
+
+//console.log(exA)
+console.log('\n')
+console.log(exA.ola)
+console.log(exA.bemVindo)
+console.log(exA.ateLogo)
+
+//console.log(exB)
+console.log('\n')
+console.log(exB.bomDia)
+console.log(exB.boaNoite())
