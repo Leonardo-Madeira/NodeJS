b23932492637cdf39c4eb8df6cf478a889496578
Leonardo Madeira <85585129+Leonardo-Madeira@users.noreply.github.com>
2021-07-31 23:32:59 -0300

    Update index.js

--- 01 - PrimeiroNode/index.js
+++ 01 - PrimeiroNode/index.js
@@ -1,7 +1,8 @@
 //Primeiro uso com node
 
-let somar = (x,y) => x+y // função para somar x+y
+let somar = (x, y) => x + y // função para somar x+y
 
-setInterval(function(){ // função que vai retornar a função somar a cada 2s
-  console.log(somar(50,50));
-},2000)
+setInterval(function () {
+  // função que vai retornar a função somar a cada 2s
+  console.log(somar(50, 50))
+}, 2000)
