43b89067d697a97d1085e7b51ddbfb05118cfd6d
Leonardo Madeira <85585129+Leonardo-Madeira@users.noreply.github.com>
2021-08-04 23:14:42 -0300

    Update ex=a.js

--- 07 - Modulos/modulo - 01/ex=a.js
+++ 07 - Modulos/modulo - 01/ex=a.js
@@ -1,5 +1,5 @@
 // Tres formas de exportar um objeto, todos os exemplos abaixo é um objeto.
 
-this.ola = 'Fala pessoal';
-exports.bemVindo = 'Bem vindo ao nodejs!';
-module.exports.ateLogo = 'Até a próxima!';
+this.ola = 'Fala pessoal'
+exports.bemVindo = 'Bem vindo ao nodejs!'
+module.exports.ateLogo = 'Até a próxima!'
