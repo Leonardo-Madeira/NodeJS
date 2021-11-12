b0600780d526673e5eec18fd7fbe27c8575c64d4
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-11 23:53:54 -0300

    Update index.js

--- Aulas-iniciante/01 - PrimeiroNode/index.js
+++ Aulas-iniciante/01 - PrimeiroNode/index.js
@@ -5,4 +5,4 @@ let somar = (x, y) => x + y // função para somar x+y
 setInterval(function () {
   // função que vai retornar a função somar a cada 2s
   console.log(somar(50, 50))
-}, 2000)
+}, 2000) // 2.000(dois mil) é 2s(dois segundos) em nodejs
