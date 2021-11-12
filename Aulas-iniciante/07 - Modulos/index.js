b3db44b14ede85157dc35cf8a38fc64f7957e504
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-11 23:57:54 -0300

    Update index.js

--- Aulas-iniciante/07 - Modulos/index.js
+++ Aulas-iniciante/07 - Modulos/index.js
@@ -1,4 +1,4 @@
 const modulo = require('./pasta') // variavel teste esta pegando a resposta da pasta teste01
 
-modulo.myFunction() // esta buscando a função mensagem que esta dentro da pasta teste
-console.log(modulo.num) // aqui a mesma coisa
+modulo.myFunction() // esta buscando a função mensagem que está dentro da pasta teste
+console.log(modulo.num) // aqui o faz o mesmo resultado
