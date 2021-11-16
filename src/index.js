35bdf654baabebfd0568976280bdce8065ab457c
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-16 16:32:34 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,51 @@
-const express = require('express')
+const { request } = require("express")
+const express = require("express")
 
 const app = express()
 
-app.get("/", (request, response) => {
-    return response.json({message: 'Hello, world! Ignite :P'})
+/*
+    * GET = Buscar uma informação dentro do servidor
+    * POST = Inserir uma informação no servidor
+    * PUT = Alterar uma informação no servidor
+    * PATCH = alterar uma informação específico
+    * DELETE = Deletar uma informação no servidor
+ */
+
+/**
+ Tipos de parametros:
+
+ 1 - Route Params => Indentificar um recurso editar/deletar/buscar {
+    const {id} = request.params
+    console.log(id)
+ }
+
+ 2 - Query Params => Paginação /Filtro
+
+ */
+
+app.get("/courses", (req, res) => {
+    const query = request.query
+    console.log(query)
+    return res.json(["Curso 1", "Curso 2", "Curso 3"])
+})
+
+app.post("/courses", (req, res) =>{
+    return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
+});
+
+app.put("/courses/:id", (req, res) =>{
+    const {id} = request.params
+    console.log(id)
+    return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
+
+})
+
+app.patch("/courses/:id", (req, res) =>{
+    return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
+})
+
+app.delete("/courses/:id", (req, res) =>{
+    return res.json(["Curso 6", "Curso 2", "Curso 4"])
 })
 
 app.listen(8080)
--- src/test.js
+++ src/test.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get("/", (req, res) => {
-    return res.send('Hello, world! FUCK')
+    return res.send('Hello, world!')
 })
 
 app.listen(3333)
