e31ad2db058a58785d0f02d1c03a208a53624582
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-17 13:22:29 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.json())
+
 /*
     * GET = Buscar uma informação dentro do servidor
     * POST = Inserir uma informação no servidor
@@ -11,34 +13,52 @@ const app = express()
  */
 
 /**
- Tipos de parametros:
+ #Tipos de parametros:
 
- 1 - Route Params => Indentificar um recurso editar/deletar/buscar {
-    const {id} = request.params
+ 1 - Route Params => Indentificar um recurso editar/deletar/buscar
+    app.put("/courses/:id", (req, res) =>{
+    const {id} = req.params
     console.log(id)
- }
+    return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
+})
+ 
 
  2 - Query Params => Paginação /Filtro
+    app.get("/courses", (req, res) => {
+    const query = req.query
+    console.log(query)
+    return res.json(["Curso 1", "Curso 2", "Curso 3"])
+})
+
+ 3 - Body Params => Os objetos inserção/alteração
+    app.post("/courses", (req, res) =>{
+    const body = req.body
+    console.log(body)
+    return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
+});
+
+:>  1 - Route Params => Indentificar um recurso editar/deletar/buscar
+:>  2 - Query Params => Paginação /Filtro
+:>  3 - Body Params => Os objetos inserção/alteração
 
  */
 
 app.get("/courses", (req, res) => {
-    const query = request.query
+    const query = req.query
     console.log(query)
     return res.json(["Curso 1", "Curso 2", "Curso 3"])
 })
 
 app.post("/courses", (req, res) =>{
-    const {id} = request.params
-    console.log(id)
+    const body = req.body
+    console.log(body)
     return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
 });
 
 app.put("/courses/:id", (req, res) =>{
-    const {id} = request.params
+    const {id} = req.params
     console.log(id)
     return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
-
 })
 
 app.patch("/courses/:id", (req, res) =>{
