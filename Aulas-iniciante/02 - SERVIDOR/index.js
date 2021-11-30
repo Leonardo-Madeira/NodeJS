dfb98c0d52e427e7ea03e0c6e67f119d158c7df2
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-29 22:40:49 -0300

    Update index.js

--- Aulas-iniciante/02 - SERVIDOR/index.js
+++ Aulas-iniciante/02 - SERVIDOR/index.js
@@ -5,12 +5,15 @@ const hostname = '127.0.0.1'
 const port = 8080 // pode ser a porta 8080
 
 const server = http.createServer((req, res) => {
+
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-
   res.end('Olá, mundo!')
+
 })
 
 server.listen(port, hostname, () => {
+
   console.log('Servidor está rodando!')
+
 })
