2577faf6b5e9543e022ac46477dab9ead8865ede
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-01 23:37:29 -0300

    commit

---
+++ src/Discorver/Iniciando na pratica/console.js
@@ -0,0 +1,8 @@
+// https://nodejs.org/dist/latest-v16.x/docs/api/globals.html
+console.log(global);
+
+//Fala do diretorio de onde eu estou
+console.log(__dirname) 
+
+// caminho completo e o nome do arquivo do programa
+console.log(__filename) 
