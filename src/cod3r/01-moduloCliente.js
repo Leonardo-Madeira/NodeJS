bfc22f282e778055afd6c047fc6b35c86cc05bbd
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-06 23:04:43 -0300

    Create 01-moduloCliente.js

---
+++ src/cod3r/01-moduloCliente.js
@@ -0,0 +1,17 @@
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+const mA = moduloA
+const mB = moduloB
+
+console.log(mA.ola)
+console.log(mA.bemVIndo)
+console.log(mA.ateLogo)
+//console.log(mA)
+
+console.log('')
+
+console.log(mB.bomDia)
+console.log(mB.function())
+console.log(mB.obj)
+//console.log(mB)
