bd3e8749675abb6fe4206dfd04d41e20089cfc7f
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-06 23:04:31 -0300

    Create 01-moduloB.js

---
+++ src/cod3r/01-moduloB.js
@@ -0,0 +1,9 @@
+module.exports = { // essa é a melhor forma para exportar algo
+    bomDia: 'Bom dia',
+    //arrouFunc = () => {'função'},
+    function(){
+        return 'função'
+    },
+    obj: {objeto: 'obj'},
+    //array = ['a','r','r','a','y'] 
+}
