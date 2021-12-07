6900762eb024b6841dd59918d987d5f195b65537
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-06 23:05:13 -0300

    Create 06-arquivoLeitura.js

---
+++ src/cod3r/06-arquivoLeitura.js
@@ -0,0 +1,21 @@
+const fs = require('fs')//modulo do node
+
+const caminho = __dirname + '/06-arquivo.json'
+
+//sincrono...
+const conteudo = fs.readFileSync(caminho, 'utf-8')//função que le um arquivo de forma sincrona
+console.log(conteudo)
+
+//assincrono...
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const config = JSON.parse(conteudo)
+    console.log(`${config.db.host}:${config.db.port}`)
+})
+
+const config = require('./06-arquivo.json')
+console.log(config.db)
+
+fs.readdir(__dirname, (err, arquivos) =>{
+    console.log('Conteudo da pasta...')
+    console.log(arquivos)
+})
