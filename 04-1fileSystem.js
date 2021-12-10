728f43863632cbdcde51a149fe2f12a8ac8fe161
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-09 23:04:58 -0300

    commit

---
+++ 04-1fileSystem.js
@@ -0,0 +1,18 @@
+//File System / Sistema de arquivos
+
+//O módulo de sistema de arquivos Node.js permite que você trabalhe com o sistema de arquivos em seu computador.
+
+const fs = require('fs')
+
+//Uso comum para o módulo File System:
+
+// * Ler arquivos       | Read files
+// * Criar arquivos     | Create files
+// * Atualizar arquivos | Update files
+// * Deletar arquivos   | Delete files
+// * Renomear arquivos  | Rename files
+// * Upload Files
+
+//**************************************** */
+
+
---
+++ 04-5deleteFiles.js
@@ -0,0 +1,12 @@
+// Deletar arquivos / Delete Files
+// To delete a file with the File System module,  use the fs.unlink() method.
+
+// The fs.unlink() method deletes the specified file:
+
+const fs = require('fs');
+
+fs.unlink('mynewfile2.txt', function (err) {
+  if (err) throw err;
+  console.log('File deleted!');
+});
+
