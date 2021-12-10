ecef6820c6fa531abe27cda9ea364bb8182a5682
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-12-09 23:04:29 -0300

    Create 04-6renameFIles.js

---
+++ 04-6renameFIles.js
@@ -0,0 +1,11 @@
+// Renomear arquivos / Rename Files
+// Para renomear um arquivo com o módulo Sistema de Arquivos, use o fs.rename() método.
+
+// fs.rename()método renomeia o arquivo especificado:
+
+const fs = require('fs');
+
+fs.rename('mynewfile1.txt', 'myrenamedfile.txt', (err) => {
+  if (err) throw err;
+  console.log('File Renamed!');
+});
